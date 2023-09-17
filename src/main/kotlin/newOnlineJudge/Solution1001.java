13653d8404a0f84ebe1b4e5ea2a4531994e3afb2
wuhongru <1425636954@qq.com>
2023-09-17 21:06:01 +0800

    修改了gradle 的构建 使得可以独立运行java的main函数 添加例子1

---
+++ src/main/kotlin/newOnlineJudge/Solution1001.java
@@ -0,0 +1,13 @@
+package newOnlineJudge;
+
+/**
+ * 题号 1001
+ * 题目描述：小王开始学习C语言了，他非常高兴，他要对全世界的人说“Hello. C. ”，请编程实现他的愿望。
+ * 输入：无任何输入
+ * 输出：输出一行:Hello. C.
+ */
+public class Solution1001 {
+    public static void main(String[] args) {
+        System.out.println("Hello. C.");
+    }
+}
--- src/main/kotlin/newOnlineJudge/xiaohuang.md
+++ src/main/kotlin/newOnlineJudge/xiaohuang.md
@@ -3,3 +3,4 @@
 > - 此文件夹 newOnlineJudge 命名格式 数字编号对应题号 类比 leetCode文件夹
 > - 题目来源网站 http://oj.ecustacm.cn/
 > - 建议从入门开始做
+> - 对于这个OJ 他的判题是要从Main类来判 所以建议提交代码的时候把类名改成Main
