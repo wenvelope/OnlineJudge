6b140e3a7af98b8328c26af43d2f26d5564ae44a
hys <3154275278@qq.com>
2023-09-19 20:03:32 +0800

    今日用到了新新函数scanner 遇到了新新用法hasNextLine 知道了在while中可以直接return退出Main函数

--- src/main/kotlin/newOnlineJudge/Solution1000.java
+++ src/main/kotlin/newOnlineJudge/Solution1000.java
@@ -1,6 +1,22 @@
 package newOnlineJudge;
 
+import java.util.Scanner;
+
+/**
+ * 题目描述
+ * 给定两个整数a，b(0<=a, b<=1000)，计算a+b的值。
+ * 输入格式
+ * 有多组测试数据，每组一行，每行有两个整数a，b。
+ * 输出格式
+ * 输出a+b的值
+ */
 public class Solution1000 {
     public static void main(String[] args) {
-
-    }}
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
